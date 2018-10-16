25e67d84704900a73e5515180bcc988f11991d0e
Shannon Liang <shannonliang312@gmail.com>
2018-10-16 16:08:52 +0800

    udpate

--- strlen-sizeof/main.c
+++ strlen-sizeof/main.c
@@ -4,9 +4,12 @@
 int main(int argc, char const *argv[])
 {
   char ss[15] = "liangshengnan";
+  int aa[10] = {1,2,3};
 
-  printf("sizeof ss = %ld\n", sizeof(ss));
-  printf("strlen of ss = %ld\n", strlen(ss));
+  printf("sizeof ss = %zd\n", sizeof(ss)); // sizeof标示有多少个bytes
+  printf("strlen of ss = %zd\n", strlen(ss));
+
+  printf("sizeof aa = %zd\n", sizeof(aa));
 
   return 0;
 }
