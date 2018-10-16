b8b246a8e912323cd446aa501d19206579f47a84
Shannon Liang <shannonliang312@gmail.com>
2018-10-16 15:59:57 +0800

    strlen与sizeof

---
+++ strlen-sizeof/main.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<string.h>
+
+int main(int argc, char const *argv[])
+{
+  char ss[15] = "liangshengnan";
+
+  printf("sizeof ss = %ld\n", sizeof(ss));
+  printf("strlen of ss = %ld\n", strlen(ss));
+
+  return 0;
+}
